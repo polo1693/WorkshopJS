7d0c73ffcabbf50a219d7c80f2ab631e2a0436b0
Paula Bendea <Paula.Bendea@garmin.com>
2020-01-21 10:38:30 +0200

    promise chain

--- index.js
+++ index.js
@@ -1,105 +1,3 @@
-// function callback(printable) {
-//     console.log("I'm the callback", printable, 3, "Pot orice")
-// }
-
-
-// function a (callback) {  
-//     const myConstant = 2 
-//     const myString = "I'm string"
-//     const myObject = {
-//         obj2 : {
-//             obj3: {
-
-//             }
-//         }
-//     }
-//     const myArray = [1,2,3]
-//     callback(myArray[2]) 
-// }
-
-// setTimeout(callback, 2000) //merge la referinta functiei callback
-// setTimeout(callback, 3000)
-// setTimeout(callback, 4000)
-
-// callback(2)
-
-// a(callback)  //trebuie sa avem fun de callback ca param
-
-
-// const obj = {}
-// obj.fakeA = 3 //mergee
-// obj = { fakeA: 3 } // ERROR
-
-class Example {
-    /// we dont need to define the fields, this - is an object, where we declare param1 as a key
-    constructor(param1, param2){
-        this.param1 = param1;
-        this.param2 = param2;
-    }
-
-    method1 () {
-        console.log("Method 1", this.param1);
-    }
-
-    method2 () {
-        console.log("Method 2");
-    }
-}
-
-    const myExampleInst = new Example(); ///here is the class
-    myExampleInst.method1();
-
-    const example = {
-        method1(){
-            console.log("m1");
-        },
-        method2(){
-            console.log("m2")
-        }
-    }
-
-    example.method1();   ///here is an object
-
-    console.log(example);
-
-    setTimeout(myExampleInst.method1, 3000); ///se face asincron, candva..si nu blocheaza procesul
-    console.log(example);
-
-
-    /////PROMISE////
-    
-
-    const a = 2;
-    function impl(resolve, reject){ //sincron
-      //  resolve(); ///pe resolve se stie ca promise-ul a fost executat cu success
-      //  reject(); //pe reject - se stie ca promise-ul s-a apelat, insa fara success, a fost ceva exceptie...promise-ul nu s-a indeplinit
-
-      if (a === 2){  //sincron
-          setTimeout(function() { //asincron
-              console.log("I'm finally called after 2000 ms", 2000)
-          resolve(a)
-        }, 2000);
-      } else{
-          reject()
-      }
-    }
-
-    // clasa care reda starea obiectului - are 3 stari, pending, fulfilles, rejected(cand avem ceva exceptie in interiorul promise-ului)
-    const promise = new Promise(impl);
-    //pe then se pot trimite si date ca param
-    promise.then(function (a) {   //se apeleaza cand promise-ul e gata
-        console.log("I am finally fulfilled", a);
-    })
-    console.log(promise);
-
-    // const promise = new Promise(function impl(resolve, reject){
-    // });
-
-
-    const myPromise = () => new Promise((resolve, reject) => {
-    
-    })
-
 /////*****CHALLANGE*****//////
     // successfully use the `fetch` library (DON'T LOOK IT UP ON THE INTERNET)
 /*
@@ -119,4 +17,13 @@ class Example {
     Task 2:
     wrap the JSON result in a promise, call it, and display the result
 */
+const fetch = require('node-fetch');
 
+fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+ return response.json()  // type of Response, not a json
+
+})
+.then (jsonResult => {
+    console.log(jsonResult);
+    console.log('title is', jsonResult.title);
+})
