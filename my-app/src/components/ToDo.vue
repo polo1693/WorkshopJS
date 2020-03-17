d6129687b4d2f675f0753d9b7f088d8f415bff74
Paula Bendea <Paula.Bendea@garmin.com>
2020-03-17 10:27:55 +0200

    App vue - project

---
+++ my-app/src/components/ToDo.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <!-- id-ul il generam pe baza de name si value -->
+    <input type="checkbox" :id="`${inputName}_id`" :name='inputName' :value='inputValue'>
+    <label :for="`${inputName}_id`"> {{ text }} </label>
+</div>
+</template>
+
+<script>
+export default {
+      name: 'ToDo',
+      props:{
+          text: String,
+          inputName: String,
+          inputValue: String
+}
+}
+</script>
---
+++ my-app/src/components/ToDoList.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <div>
+    {{headerName}}
+    <ToDo 
+        text = 'I have a bike'
+        input-name = 'bike'
+        input-value = 'Bike'
+    />
+    <ToDo text = 'I have a nike'/>
+    <ToDo text = 'I have a mike'/>
+    </div>
+  
+</div>
+</template>
+
+<script>
+import ToDo from './ToDo.vue'
+export default {
+      name: 'ToDoList',
+      props:{
+         headerName: String
+},
+    components:{
+        ToDo
+    }
+}
+</script>
